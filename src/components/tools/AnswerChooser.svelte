8a357daf4362a49653e81b48c2af7da9d3317560
Skayo <jonas@skayo.dev>
2020-09-18 19:29:55 +0200

    Removed Wheel of Fortune and switched to a simpler, slot-machine-like Answer Chooser

--- src/components/tools/AnswerChooser.svelte
+++ src/components/tools/AnswerChooser.svelte
@@ -1,8 +1,6 @@
-<script>
-	import WheelOfFortune from '../misc/WheelOfFortune.svelte';
-	import Tool from './Tool.svelte';
-
-	const answers = {
+<script context="module">
+	const SLIDE_LIST_LENGTH = 5;
+	const ANSWERS = {
 		'Deine Mama':                                    'LittleFreakBe',
 		'Schöne Schuhe hast du':                         'dingusbread44',
 		'normal ist das nicht':                          'Klirrkopf',
@@ -39,37 +37,99 @@
 		'hab gehört das is recht ungesund':              'Skayo',
 		'Das auch besser so, du':                        'dingusbread44',
 	};
+</script>
 
-	let complete = false;
-	let resultField;
-	let result = 'wenn du verstehst was ich meine';
+<script>
+	import { tick } from 'svelte';
+	import Tool from './Tool.svelte';
+
+	let slotItems = ['Click here to spin!'];
+
+	let slotMachineContainer;
+	let firstSpin = true;
+	let result = '';
 
 	export let copyText;
 
-	function handleSpinComplete(_result) {
-		result = _result;
-		complete = true;
+	function makeSlotItems() {
+		slotItems = [];
+
+		for (let i = 0; i <= SLIDE_LIST_LENGTH; i++) {
+			slotItems = [...slotItems, ...Object.keys(ANSWERS)];
+		}
 	}
 
-	function select(element) {
-		element.select();
-		element.setSelectionRange(0, element.value.length);
+	function randomAnswerElement() {
+		const answerElements = slotMachineContainer.children;
+
+		const randomIndex = Math.floor(Math.random() * answerElements.length);
+
+		return answerElements[randomIndex];
+	}
+
+	async function handleClick() {
+		if (firstSpin) {
+			firstSpin = false;
+
+			makeSlotItems();
+
+			await tick();
+		}
+
+		// Play animation
+		const answerElementToScrollTo = randomAnswerElement();
+		slotMachineContainer.style.top = (-answerElementToScrollTo.offsetTop) + 'px';
+		result = answerElementToScrollTo.innerText.trim();
 	}
 </script>
 
 <Tool title="Answer Chooser (German)" size="large">
-	<WheelOfFortune prizes={Object.keys(answers)} onComplete={handleSpinComplete} />
-
-	{#if complete}
-		<div class="uk-width-1-1 uk-flex uk-flex-center uk-margin-bottom">
-			<span class="uk-width-auto uk-hiddden@s" data-uk-icon="icon: arrow-down; ratio: 4" data-uk-tooltip="pos: left" title="Result"></span>
+	<div on:click={handleClick} class="slot-machine">
+		<div bind:this={slotMachineContainer} class="slot-machine-container">
+			{#each slotItems as slotItem}
+				<div class="slot-machine-item">
+					<h1 class="slot-machine-item-title">{slotItem}</h1>
+				</div>
+			{/each}
 		</div>
+	</div>
 
-		<div class="uk-text-center">
-			<input bind:this={resultField} on:click={select(resultField)} class="uk-input uk-form-large" type="text" readonly value={result}>
-			<a class="uk-text-meta uk-margin-small-top" href="https://twitter.com/{answers[result]}">by @{answers[result]}</a>
-		</div>
-
+	{#if !firstSpin}
 		<button on:click={copyText(result)} class="uk-button uk-button-primary uk-width-1-1 uk-margin-small-top">Copy</button>
 	{/if}
 </Tool>
+
+<style lang="scss">
+	$slot-machine-item-height: 80px;
+
+	.slot-machine {
+		position: relative;
+		width: 100%;
+		overflow: hidden;
+		height: $slot-machine-item-height;
+		cursor: pointer;
+
+		&-container {
+			transition: top ease-in-out 0.5s;
+			position: absolute;
+			width: 100%;
+			top: 0;
+		}
+
+		&-item {
+			height: $slot-machine-item-height;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			white-space: nowrap;
+
+			&-title {
+				margin: 0 !important;
+				font-weight: bold;
+				overflow: hidden;
+				text-overflow: ellipsis;
+				line-height: $slot-machine-item-height;
+			}
+		}
+	}
+</style>
